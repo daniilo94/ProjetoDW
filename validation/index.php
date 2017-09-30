018d42cb3e5b36842ddc833b541cb5ce8b899fb6
daniilo94 <30841367+daniilo94@users.noreply.github.com>
2017-09-30 18:37:27 -0300

    Add files via upload

---
+++ validation/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso de Ajax</title>
+    </head>
+    <body>
+        <input type="file" id="fotografia"/>
+
+        <script src="script.js" type="text/javascript"></script>
+    </body>
+</html>
